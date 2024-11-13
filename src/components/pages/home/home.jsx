98a4b407236a43de991958c57f363c5a0a4b6d7e
git <florah@gmail.com>
2024-11-13 22:02:41 +0300

    solve import errors

--- src/components/pages/home/home.jsx
+++ src/components/pages/home/home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Search from "../../search/search";
 import Results from "../../results/results";
-import "./Home.css";
+import "./home.css";
 
 function Home() {
   return (
